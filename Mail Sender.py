fe2097a1a49006b486f9663e268de850551fadc4
The-Robin-Hood <32297581+The-Robin-Hood@users.noreply.github.com>
2019-04-16 17:53:24 +0530

    Update Mail Sender.py

--- Mail Sender.py
+++ Mail Sender.py
@@ -2,24 +2,24 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-username = raw_input("Enter the valid username of smtp2go")
-password = raw_input("Enter the password")
+username = raw_input("Enter the valid username of smtp2go: ")
+password = raw_input("Enter the password: ")
 msg = MIMEMultipart('mixed')
 
-sender = raw_input("Enter the Sender Email ID :")
-No_of_recipient= int(raw_input("Enter the Number of recipients:"))
+sender = raw_input("Enter the Sender Email ID : ")
+No_of_recipient= int(raw_input("Enter the Number of recipients: "))
 if (No_of_recipient == 1):
-    recipient = raw_input("Enter the Recipient Email ID:")
+    recipient = raw_input("Enter the Recipient Email ID: ")
 elif (No_of_recipient>1):
     recipient=[]
     for i in  range(No_of_recipient):      
-        recipient.append(raw_input("Enter the Recipients:"))
+        recipient.append(raw_input("Enter the Recipients: "))
 else:
     print("Enter a Valid Number")
-msg['Subject'] = raw_input("Enter the Subject:")
+msg['Subject'] = raw_input("Enter the Subject: ")
 msg['From'] = sender
 msg['To'] = ",".join(recipient)
-Format= raw_input("Enter the format of the mail P or H(Plain OR Html):")
+Format= raw_input("Enter the format of the mail P or H(Plain OR Html): ")
 P ="P"
 H="H"
 if (Format == P):
